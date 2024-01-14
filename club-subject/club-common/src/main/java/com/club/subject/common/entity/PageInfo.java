6cdfa2341a0d9a65ee370564f7f5deb16602fac5
gao <igao107@163.com>
2024-01-14 11:58:41 +0800

    feature: 增加分页信息

---
+++ club-subject/club-common/src/main/java/com/club/subject/common/entity/PageInfo.java
@@ -0,0 +1,31 @@
+package com.club.subject.common.entity;
+
+import lombok.Data;
+
+/**
+ * 分页请求信息
+ *
+ * @author serendipity
+ * @version 1.0
+ * @date 2024/1/14
+ **/
+public class PageInfo {
+
+    private Integer pageNo = 1;
+
+    private Integer pageSize = 20;
+
+    public Integer getPageNo() {
+        if (pageNo == null || pageNo < 1) {
+            return 1;
+        }
+        return pageNo;
+    }
+
+    public Integer getPageSize() {
+        if (pageSize == null || pageSize < 1 || pageSize > Integer.MAX_VALUE) {
+            return 20;
+        }
+        return pageSize;
+    }
+}
